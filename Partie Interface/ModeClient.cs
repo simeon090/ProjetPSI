d55fabf3c504ea2683d709db08bbb63b894c3dce
yanis928 <163989530+yanis928@users.noreply.github.com>
2025-05-04 16:56:56 +0200

    Add files via upload
    ajouter un met une seule fenetre et retoure en arrière avec une flèche

---
+++ Partie Interface/ModeClient.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LivInParis
+{
+    public partial class ModeClient : Form
+    {
+        public ModeClient()
+        {
+            InitializeComponent();
+            this.BackColor = Color.LightBlue;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Passer_commande commande = new Passer_commande();
+            commande.ShowDialog();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            ChoixMode choixMode = new ChoixMode();
+            this.Hide();
+            choixMode.ShowDialog(); 
+        }
+    }
+}
---
+++ Partie Interface/Modifier un met.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LivInParis.Partie_Interface
+{
+    public partial class Modifier_un_met : Form
+    {
+        public Modifier_un_met()
+        {
+            InitializeComponent();
+            this.BackColor = Color.LightBlue;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            ModeCuisinier modeCuisinier = new ModeCuisinier();
+            this.Hide();
+            modeCuisinier.ShowDialog(); 
+        }
+    }
+}
